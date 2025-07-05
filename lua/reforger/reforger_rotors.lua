9770184bdff6945d4b7754f98f43fa2b51693bb1
RareIridium77 <219307991+RareIridium77@users.noreply.github.com>
2025-07-05 23:16:00 +0500

    Minor changes and optimization

--- lua/reforger/reforger_rotors.lua
+++ lua/reforger/reforger_rotors.lua
@@ -6,7 +6,7 @@ function Reforger.RotorsGetDamage(veh, dmginfo)
     if not IsValid(veh) then return end
 
     local rotor = Reforger.FindRotorsAlongRay(veh, dmginfo)
-    print(rotor)
+
     if not IsValid(rotor) then return end
 
     if rotor.rotorHealth == nil then
@@ -15,7 +15,7 @@ function Reforger.RotorsGetDamage(veh, dmginfo)
 
     rotor.rotorHealth = rotor.rotorHealth - dmginfo:GetDamage() / 2
 
-    if rotor.rotorHealth <= 0 and rotor.Destroy then
+    if rotor.rotorHealth <= 0 and isfunction(rotor.Destroy) then
         rotor:Destroy()
         Reforger.DevLog("Rotor destroyed: " .. tostring(rotor))
     end
@@ -24,41 +24,12 @@ end
 function Reforger.FindRotorsAlongRay(veh, dmginfo)
     if not IsValid(veh) then return nil end
 
-    local Len = veh:BoundingRadius()
-    local dmgPos = dmginfo:GetDamagePosition()
-    local dmgDir = dmginfo:GetDamageForce():GetNormalized()
-
-    if dmgDir:Length() == 0 then return nil end
-
-    local dmgPenetration = dmgDir * Len
-    local dmgStart = dmgPos - dmgDir * (Len * 0.5)
-
-    local closestRotor = nil
-    local closestDist = Len * 2
-
     local rotors = Reforger.GetRotors(veh)
     if not istable(rotors) or #rotors == 0 then return nil end
 
-    for _, rotor in ipairs(rotors) do
-        if not IsValid(rotor) then continue end
+    local classname = rotors[1]:GetClass()
 
-        local mins, maxs = rotor:OBBMins(), rotor:OBBMaxs()
-        local pos, ang = rotor:GetPos(), rotor:GetAngles()
-
-        local HitPos = util.IntersectRayWithOBB(dmgStart, dmgDir * Len * 1.5, pos, ang, mins, maxs)
-
-        if HitPos then
-            debugoverlay.BoxAngles(pos, mins, maxs, ang, 1, Color(255, 100, 0, 50))
-
-            local dist = (HitPos - dmgPos):Length()
-            if dist < closestDist then
-                closestRotor = rotor
-                closestDist = dist
-            end
-        end
-    end
-
-    return closestRotor
+    return Reforger.FindClosestByClass(veh, dmginfo, classname)
 end
 
 function Reforger.FindRotors(veh)
@@ -73,7 +44,7 @@ function Reforger.FindRotors(veh)
         if IsValid(veh.mainRotor) then table.insert(rotors, veh.mainRotor) end
         if IsValid(veh.tailRotor) then table.insert(rotors, veh.tailRotor) end
 
-        if #rotors == 0 and (vehicle_type == "plane" or vehicle_type == "helicopter") then
+        if #rotors == 0 and vehicle_type == "plane" then
             rotors = Reforger.PairEntityAll(veh, "glide_rotor")
         end
     end
@@ -93,7 +64,7 @@ function Reforger.CacheRotors(veh)
 
     local vehicle_type = Reforger.GetVehicleType(veh)
     local vehicle_base = Reforger.GetVehicleBase(veh)
-    if vehicle_base ~= "simfphys" then return end
+    if vehicle_base == "simfphys" then return end
     if vehicle_type ~= "helicopter" or vehicle_type ~= "plane" then return end
 
     timer.Simple(0, function()
