5d772b5c0c692222a66a6b11cb4e70ad996e0785
RareIridium77 <219307991+RareIridium77@users.noreply.github.com>
2025-07-13 00:49:14 +0500

    Fixed bullet callback for lvs

--- lua/reforger/modules/server/lvs/reforger_lvs_bulletsystem.lua
+++ lua/reforger/modules/server/lvs/reforger_lvs_bulletsystem.lua
@@ -52,11 +52,11 @@ hook.Add("Reforger.Init", "LVS_Reforger.ChangeBulletFire", function()
                 end
             end
 
-            bullet.Callback = function( self, attacker, trace, dmginfo )
-                hook.Run("Reforger.LVS_BulletCallback", self, attacker, trace, dmginfo)
+            bullet.Callback = function( attacker, trace, dmginfo )
+                hook.Run("Reforger.LVS_BulletCallback", bullet, attacker, trace, dmginfo)
 
                 if bulletCallback then
-                    bulletCallback( self, attacker, trace, dmginfo )
+                    bulletCallback( attacker, trace, dmginfo )
                 end
             end
 
