ab2ab56b1b0cfee1d8ee4e3eed44ffe2048ad2f2
RareIridium77 <219307991+RareIridium77@users.noreply.github.com>
2025-08-10 19:03:16 +0500

    Changes: moved glide server modules to one file: sv_reforger_glide.lua

--- lua/reforger/modules/server/glide/sv_reforger_glide.lua
+++ lua/reforger/modules/server/glide/sv_reforger_glide.lua
@@ -1,4 +1,35 @@
--- Fixes Aim steering for Glide
+--- [ Reforger Glide Recieve Offsets ] ---
+
+util.AddNetworkString("Reforger.SendGlideOffsets")
+
+net.Receive("Reforger.SendGlideOffsets", function(_, ply)
+    local ent = net.ReadEntity()
+    if not IsValid(ent) or not ent.IsGlideVehicle then return end
+
+    local count = net.ReadUInt(8)
+    if count <= 0 or count > 16 then
+        Reforger.Log("Blocked suspicious offset packet from " .. tostring(ply))
+        return
+    end
+
+    local offsets = {}
+
+    for i = 1, count do
+        local offset = net.ReadVector()
+
+        if not isvector(offset) then
+            Reforger.Log("Received non-vector offset. Player: " .. tostring(ply))
+            return
+        end
+
+        offsets[i] = { offset = offset }
+    end
+
+    ent.EngineFireOffsets = offsets
+end)
+
+
+--- [ Reforger Glide Fix Pod ] ---
 local function Glide_ActivateFixator()
     local EntityMeta = FindMetaTable( "Entity" )
     local PlayerMeta = FindMetaTable( "Player" )
